57503a34fd8f394b5260f5ae3d933d4844d6cbb0
Lou Carther <dieudrichard@gmail.com>
2024-01-18 12:44:14 +0100

    change version and adjust name

--- app/src/components/_layout/Header/index.tsx
+++ app/src/components/_layout/Header/index.tsx
@@ -1,5 +1,5 @@
+import { Flex, Text } from "@radix-ui/themes";
 import NextLink from "next/link";
-import { Container, Flex, Text } from "@radix-ui/themes";
 import NavBar from "../NavBar";
 
 const LayoutHeader = () => {
@@ -9,7 +9,7 @@ const LayoutHeader = () => {
         <NextLink className="font-bold text-lg md:text-2xl" href={"/"}>
           <Text className="">RD</Text>
           <Text className="text-sm text-zinc-400" ml={"2"}>
-            Beta v0.1.0
+            Alpha v0.1.0
           </Text>
         </NextLink>
         <NavBar />
