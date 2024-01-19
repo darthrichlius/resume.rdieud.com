047dbf146f6b14b1f6de906c22b430dccf81a71f
Lou Carther <dieudrichard@gmail.com>
2024-01-19 20:35:49 +0100

    feat(SEO): Add custom page title and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import {
   HomeIntroductionSection,
   HomePresentationSection,
@@ -5,6 +6,13 @@ import {
   HomeResumeSection,
   HomePortfolioSection,
 } from "@@/Home";
+
+export const metadata: Metadata = {
+  title:
+    "Richard Dieud - Senior Software Engineer & Product Manager — Richard Dieud",
+  description:
+    "Fullstack Web Engineer & Product Manager passionate about creating application systems and valuable products people love",
+};
 
 export default function Home() {
   return (
