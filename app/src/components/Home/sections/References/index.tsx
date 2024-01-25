e64c0bb222ba1d92ebc3f9973e671d185c33041e
Lou Carther <dieudrichard@gmail.com>
2024-01-25 17:16:45 +0100

    feat(HomeReferences): Implement section spacing

--- app/src/components/Home/sections/References/index.tsx
+++ app/src/components/Home/sections/References/index.tsx
@@ -1,9 +1,22 @@
+"use client";
+import classNames from "classnames";
+
 import WorkReferenceCard from "./_/WorkReferenceCard";
 import workReferencesMap from "@/data/map/workReferencesMap";
+import { useWindow } from "@@src/context";
 
 const HomeReferencesSection = () => {
+  const { isLandScape, isLgVertical } = useWindow();
+
   return (
-    <section id="references" className="home-section flex justify-center">
+    <section
+      id="references"
+      className={classNames({
+        ["home-section"]: true,
+        ["window-is-landscape"]: isLandScape,
+        ["window-is-lg-vertical"]: isLgVertical,
+      })}
+    >
       <div className={"app-container"}>
         <div className="flex gap-32 flex-col items-center">
           {workReferencesMap.map((reference, i) => (
