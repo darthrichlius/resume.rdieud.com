bd5342859f6dcd2097f3ff9a90764e43b4520a79
Lou Carther <dieudrichard@gmail.com>
2024-01-28 12:17:52 +0100

    fix deployment

--- app/sandbox/component/page.tsx
+++ app/sandbox/component/page.tsx
@@ -1,18 +1,30 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import { SkillIcon } from "@@src/components";
 import { useSearchParams } from "next/navigation";
+import { TSkillIconIndex } from "@@src/components/_layout/SkillIcon";
 
 const SandBoxComponentTestPage = () => {
+  return (
+    <div>
+      {/* @see https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
+      <Suspense>
+        <SearchSkillIcon />
+      </Suspense>
+    </div>
+  );
+};
+
+const SearchSkillIcon = () => {
   const params = useSearchParams();
   const qParamIndex = params.get("index");
   const qParamTheme = params.get("theme");
 
   return (
-    <div>
-      {/* @ts-expect-error */}
-      <SkillIcon index={qParamIndex} theme={qParamTheme} />
-    </div>
+    <SkillIcon
+      index={qParamIndex as TSkillIconIndex}
+      theme={qParamTheme as "light" | "dark"}
+    />
   );
 };
 
